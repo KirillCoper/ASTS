456e35b9f59c540c6cb25671168e22f3d75b0371
KirillCoper <39758310+KirillCoper@users.noreply.github.com>
2021-01-24 01:07:23 +0700

    Добавление шапки

--- Лаб1_Григорьев_GPIO/src/main.c
+++ Лаб1_Григорьев_GPIO/src/main.c
@@ -1,3 +1,18 @@
+/***********************************************************************************************************************************
+* Copyright (C) 2021 TUSUR, FB
+* File              : main.c
+* Compiler          : gcc
+* IDE               : Keil IDE
+* Version           : 5.33
+* Created File      : 20.01.2021
+* Last modified     : 20.01.2021
+*
+* Support mail      : kirill.grigoryev.1998@gmail.ru
+*
+* Target MCU        : any
+* 
+* Description       : LED illumination by pressing a button
+***********************************************************************************************************************************/
 #include "clk.h"
 #include "led.h"
 #include "button.h"
